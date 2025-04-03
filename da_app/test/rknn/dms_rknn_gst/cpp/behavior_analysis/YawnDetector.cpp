14c4c7728a1ebab4ad3d0abb6827a4661cfa11b2
limon100 <mahmudurlimon41@gmail.com>
2025-04-04 00:09:23 +0600

    changes headpose

--- da_app/test/rknn/dms_rknn_gst/cpp/behavior_analysis/YawnDetector.cpp
+++ da_app/test/rknn/dms_rknn_gst/cpp/behavior_analysis/YawnDetector.cpp
@@ -1,3 +1,148 @@
+// #include "YawnDetector.hpp"
+// #include <iostream>
+// #include <opencv2/imgproc.hpp>
+// #include <chrono> 
+
+// YawnDetector::YawnDetector() :
+//     MOUTH_TOP(13),
+//     MOUTH_BOTTOM(14),
+//     YAWN_THRESHOLD(25),
+//     MIN_YAWN_DURATION(0.3),
+//     YAWN_COOLDOWN(0.1),
+//     is_yawn(false),
+//     yawn_start_time(0),
+//     last_yawn_time(0),
+//     yawn_kss(0),
+//     yawn_frequency(0),
+//     last_yawn_duration(0), // Add this line 
+//     reset_interval(60),
+//     last_reset_time(0)
+// {
+//     yawns.push_back(std::map<std::string, double>{{"start", 0}, {"end", 0}, {"duration", 0}});
+//     last_reset_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+// }
+
+// // void YawnDetector::_draw_annotations(const cv::Mat& frame, cv::Point mouth_top, cv::Point mouth_bottom) {
+// //     cv::Mat frameCopy = frame.clone();
+// //     int h = frameCopy.rows;
+// //     int w = frameCopy.cols;
+// //     int x1 = mouth_top.x;
+// //     int y1 = mouth_top.y;
+// //     int x2 = mouth_bottom.x;
+// //     int y2 = mouth_bottom.y;
+
+// //     // Draw mouth line
+// //     cv::line(frameCopy, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2);
+// //     cv::circle(frameCopy, cv::Point(x1, y1), 3, cv::Scalar(0, 0, 255), -1);
+// //     cv::circle(frameCopy, cv::Point(x2, y2), 3, cv::Scalar(0, 0, 255), -1);
+
+// //     if (yawn_start_time != 0) {
+// //         cv::putText(frameCopy, "Yawning", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 0), 2);
+
+// //         double progress = std::min((std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) - yawn_start_time) / MIN_YAWN_DURATION, 1.0);
+// //         cv::rectangle(frameCopy, cv::Point(10, 50), cv::Point(150, 70), cv::Scalar(0, 0, 0), -1);
+// //         cv::rectangle(frameCopy, cv::Point(10, 50), cv::Point(50 + static_cast<int>(100 * progress), 70), cv::Scalar(0, 255, 255), -1);
+// //     }
+// // }
+
+// int YawnDetector::calculateYawnKSS() {
+//     if (yawn_frequency < 3) {
+//         yawn_kss = 1;
+//     } else if (yawn_frequency < 3 && yawn_frequency < 5) {
+//         yawn_kss = 4;
+//     } else if (yawn_frequency >= 5 && yawn_frequency < 7) {
+//         yawn_kss = 7;
+//     } else if (last_yawn_duration > 2 || yawn_frequency > 7) {
+//         yawn_kss = 9;
+//     }
+//     return yawn_kss;
+// }
+
+// void YawnDetector::resetKSS() {
+//     double current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+//     if (current_time - last_reset_time >= reset_interval) {
+//         yawns.clear();
+//         yawns.push_back(std::map<std::string, double>{{"start", 0}, {"end", 0}, {"duration", 0}});
+//         yawn_kss = 1;
+//         last_reset_time = current_time;
+//     }
+// }
+
+// // double YawnDetector::calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, cv::Size frame_size) {
+// double YawnDetector::calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, int frame_width, int frame_height) {
+//     double x1 = landmark1.x;
+//     double y1 = landmark1.y;
+//     double x2 = landmark2.x;
+//     double y2 = landmark2.y;
+//     return std::hypot(x2 - x1, y2 - y1);
+// }
+
+// double YawnDetector::calculateYawnFrequency(double current_time) {
+//     while (!yawn_timestamps.empty() && current_time - yawn_timestamps.front() > 60) {
+//         yawn_timestamps.pop_front();
+//     }
+//     yawn_frequency = yawn_timestamps.size();
+//     return yawn_frequency;
+// }
+
+// // YawnDetector::YawnMetrics YawnDetector::run(const cv::Mat& frame, const std::vector<cv::Point>& faceLandmarks) {
+// YawnDetector::YawnMetrics YawnDetector::run(const std::vector<cv::Point>& faceLandmarks, int frame_width, int frame_height){
+//     YawnMetrics metrics;
+//     auto current_time = std::chrono::system_clock::now(); // Store current time
+
+//     if (faceLandmarks.size() > 10){
+//         cv::Point mouth_top = faceLandmarks[MOUTH_TOP];
+//         cv::Point mouth_bottom = faceLandmarks[MOUTH_BOTTOM];
+
+//         // Calculate mouth openness
+//         // double mouth_distance = calculatePixelDistance(mouth_top, mouth_bottom, frame.size());
+//         double mouth_distance = calculatePixelDistance(mouth_top, mouth_bottom, frame_width, frame_height);
+
+//         // Yawn detection logic
+//         if (mouth_distance > YAWN_THRESHOLD) {
+//             if (yawn_start_time == 0) {
+//                 yawn_start_time = std::chrono::system_clock::to_time_t(current_time); // Store start time in system_clock::time_point
+//                 is_yawn = true;
+//             }
+//         } else {
+//             if (yawn_start_time != 0) {
+//                 auto end_time = current_time;
+//                 std::chrono::duration<double> yawn_duration = end_time - std::chrono::system_clock::from_time_t(yawn_start_time);
+//                 last_yawn_duration = yawn_duration.count();
+
+//                 if (last_yawn_duration >= MIN_YAWN_DURATION) {
+
+//                     if ((std::chrono::system_clock::to_time_t(current_time) - last_yawn_time) > YAWN_COOLDOWN) {
+                        
+//                         std::map<std::string, double> yawn_data;
+//                         yawn_data["start"] = yawn_start_time;
+//                         yawn_data["end"] = std::chrono::system_clock::to_time_t(current_time);;
+//                         yawn_data["duration"] = last_yawn_duration;
+//                         yawns.push_back(yawn_data);
+
+//                         yawn_timestamps.push_back(std::chrono::system_clock::to_time_t(current_time));
+//                         last_yawn_time = std::chrono::system_clock::to_time_t(current_time);
+//                     }
+//                 }
+
+//                 yawn_start_time = 0;
+//                 is_yawn = false;
+//             }
+//         }
+//         // Draw landmarks and info
+//         // _draw_annotations(frame, mouth_top, mouth_bottom);
+//         yawn_frequency = calculateYawnFrequency(std::chrono::system_clock::to_time_t(current_time));
+
+//     }
+//     int yawn_kss_ = calculateYawnKSS();
+//     metrics.isYawning = is_yawn;
+//     metrics.yawnCount = yawn_frequency;
+//     metrics.yawnDuration = last_yawn_duration; 
+//     metrics.yawnFrequency = yawn_frequency;
+//     metrics.yawnKSS = yawn_kss_;
+//     return metrics;
+// }
+
 #include "YawnDetector.hpp"
 #include <iostream>
 #include <opencv2/imgproc.hpp>
@@ -14,43 +159,21 @@ YawnDetector::YawnDetector() :
     last_yawn_time(0),
     yawn_kss(0),
     yawn_frequency(0),
-    last_yawn_duration(0), // Add this line 
+    last_yawn_duration(0),
     reset_interval(60),
-    last_reset_time(0)
+    last_reset_time(0),
+    totalYawnCount(0)  // Initialize total yawn count
 {
     yawns.push_back(std::map<std::string, double>{{"start", 0}, {"end", 0}, {"duration", 0}});
     last_reset_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 }
 
-// void YawnDetector::_draw_annotations(const cv::Mat& frame, cv::Point mouth_top, cv::Point mouth_bottom) {
-//     cv::Mat frameCopy = frame.clone();
-//     int h = frameCopy.rows;
-//     int w = frameCopy.cols;
-//     int x1 = mouth_top.x;
-//     int y1 = mouth_top.y;
-//     int x2 = mouth_bottom.x;
-//     int y2 = mouth_bottom.y;
-
-//     // Draw mouth line
-//     cv::line(frameCopy, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2);
-//     cv::circle(frameCopy, cv::Point(x1, y1), 3, cv::Scalar(0, 0, 255), -1);
-//     cv::circle(frameCopy, cv::Point(x2, y2), 3, cv::Scalar(0, 0, 255), -1);
-
-//     if (yawn_start_time != 0) {
-//         cv::putText(frameCopy, "Yawning", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 0), 2);
-
-//         double progress = std::min((std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) - yawn_start_time) / MIN_YAWN_DURATION, 1.0);
-//         cv::rectangle(frameCopy, cv::Point(10, 50), cv::Point(150, 70), cv::Scalar(0, 0, 0), -1);
-//         cv::rectangle(frameCopy, cv::Point(10, 50), cv::Point(50 + static_cast<int>(100 * progress), 70), cv::Scalar(0, 255, 255), -1);
-//     }
-// }
-
 int YawnDetector::calculateYawnKSS() {
     if (yawn_frequency < 3) {
         yawn_kss = 1;
-    } else if (yawn_frequency < 3 && yawn_frequency < 5) {
+    } else if (yawn_frequency < 5) {  // Fixed range overlap
         yawn_kss = 4;
-    } else if (yawn_frequency >= 5 && yawn_frequency < 7) {
+    } else if (yawn_frequency < 7) {
         yawn_kss = 7;
     } else if (last_yawn_duration > 2 || yawn_frequency > 7) {
         yawn_kss = 9;
@@ -65,10 +188,10 @@ void YawnDetector::resetKSS() {
         yawns.push_back(std::map<std::string, double>{{"start", 0}, {"end", 0}, {"duration", 0}});
         yawn_kss = 1;
         last_reset_time = current_time;
+        // Note: totalYawnCount is NOT reset here to keep it cumulative
     }
 }
 
-// double YawnDetector::calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, cv::Size frame_size) {
 double YawnDetector::calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, int frame_width, int frame_height) {
     double x1 = landmark1.x;
     double y1 = landmark1.y;
@@ -85,23 +208,19 @@ double YawnDetector::calculateYawnFrequency(double current_time) {
     return yawn_frequency;
 }
 
-// YawnDetector::YawnMetrics YawnDetector::run(const cv::Mat& frame, const std::vector<cv::Point>& faceLandmarks) {
-YawnDetector::YawnMetrics YawnDetector::run(const std::vector<cv::Point>& faceLandmarks, int frame_width, int frame_height){
+YawnDetector::YawnMetrics YawnDetector::run(const std::vector<cv::Point>& faceLandmarks, int frame_width, int frame_height) {
     YawnMetrics metrics;
-    auto current_time = std::chrono::system_clock::now(); // Store current time
+    auto current_time = std::chrono::system_clock::now();
 
-    if (faceLandmarks.size() > 10){
+    if (faceLandmarks.size() > 14) {  // Ensure enough landmarks
         cv::Point mouth_top = faceLandmarks[MOUTH_TOP];
         cv::Point mouth_bottom = faceLandmarks[MOUTH_BOTTOM];
 
-        // Calculate mouth openness
-        // double mouth_distance = calculatePixelDistance(mouth_top, mouth_bottom, frame.size());
         double mouth_distance = calculatePixelDistance(mouth_top, mouth_bottom, frame_width, frame_height);
 
-        // Yawn detection logic
         if (mouth_distance > YAWN_THRESHOLD) {
             if (yawn_start_time == 0) {
-                yawn_start_time = std::chrono::system_clock::to_time_t(current_time); // Store start time in system_clock::time_point
+                yawn_start_time = std::chrono::system_clock::to_time_t(current_time);
                 is_yawn = true;
             }
         } else {
@@ -111,34 +230,29 @@ YawnDetector::YawnMetrics YawnDetector::run(const std::vector<cv::Point>& faceLa
                 last_yawn_duration = yawn_duration.count();
 
                 if (last_yawn_duration >= MIN_YAWN_DURATION) {
-
                     if ((std::chrono::system_clock::to_time_t(current_time) - last_yawn_time) > YAWN_COOLDOWN) {
-                        
                         std::map<std::string, double> yawn_data;
                         yawn_data["start"] = yawn_start_time;
-                        yawn_data["end"] = std::chrono::system_clock::to_time_t(current_time);;
+                        yawn_data["end"] = std::chrono::system_clock::to_time_t(current_time);
                         yawn_data["duration"] = last_yawn_duration;
                         yawns.push_back(yawn_data);
-
                         yawn_timestamps.push_back(std::chrono::system_clock::to_time_t(current_time));
                         last_yawn_time = std::chrono::system_clock::to_time_t(current_time);
+                        totalYawnCount++;  // Increment total yawn count
                     }
                 }
-
                 yawn_start_time = 0;
                 is_yawn = false;
             }
         }
-        // Draw landmarks and info
-        // _draw_annotations(frame, mouth_top, mouth_bottom);
         yawn_frequency = calculateYawnFrequency(std::chrono::system_clock::to_time_t(current_time));
-
     }
+
     int yawn_kss_ = calculateYawnKSS();
     metrics.isYawning = is_yawn;
-    metrics.yawnCount = yawn_frequency;
-    metrics.yawnDuration = last_yawn_duration; 
-    metrics.yawnFrequency = yawn_frequency;
+    metrics.yawnCount = totalYawnCount;  // Total yawns since start
+    metrics.yawnFrequency = yawn_frequency;  // Yawns in last 60 seconds
+    metrics.yawnDuration = last_yawn_duration;
     metrics.yawnKSS = yawn_kss_;
     return metrics;
 }
--- da_app/test/rknn/dms_rknn_gst/cpp/behavior_analysis/YawnDetector.hpp
+++ da_app/test/rknn/dms_rknn_gst/cpp/behavior_analysis/YawnDetector.hpp
@@ -1,3 +1,59 @@
+// #ifndef YAWNDETECTOR_HPP
+// #define YAWNDETECTOR_HPP
+
+// #include <iostream>
+// #include <vector>
+// #include <map>
+// #include <deque>
+// #include <chrono>
+// #include <opencv2/opencv.hpp>
+
+// class YawnDetector {
+// public:
+//     YawnDetector();
+
+//     struct YawnMetrics {
+//         bool isYawning;
+//         double yawnCount;
+//         double yawnDuration;
+//         double yawnFrequency;
+//         int yawnKSS;
+//     };
+
+//     // YawnMetrics run(const cv::Mat& frame, const std::vector<cv::Point>& faceLandmarks);
+//     YawnMetrics run(const std::vector<cv::Point>& faceLandmarks, int frame_width, int frame_height);
+
+// private:
+//     // void _draw_annotations(const cv::Mat& frame, cv::Point mouth_top, cv::Point mouth_bottom);
+//     int calculateYawnKSS();
+//     void resetKSS();
+//     // double calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, cv::Size frame_size);
+//     double calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, int frame_width, int frame_height);
+//     double calculateYawnFrequency(double current_time);
+
+// private:
+//     const int MOUTH_TOP;
+//     const int MOUTH_BOTTOM;
+//     const int YAWN_THRESHOLD;
+//     const double MIN_YAWN_DURATION;
+//     const double YAWN_COOLDOWN;
+//     std::deque<double> yawn_timestamps;
+//     std::vector<std::map<std::string, double>> yawns;
+//     bool is_yawn;
+//     time_t yawn_start_time;
+//     double last_yawn_time;
+//     int yawn_kss;
+//     double yawn_frequency;
+//     double last_yawn_duration;
+//     double yawn_duration;
+//     double reset_interval;
+//     double last_reset_time;
+// };
+
+// #endif // YAWNDETECTOR_HPP
+
+
+
 #ifndef YAWNDETECTOR_HPP
 #define YAWNDETECTOR_HPP
 
@@ -14,20 +70,17 @@ public:
 
     struct YawnMetrics {
         bool isYawning;
-        double yawnCount;
+        double yawnCount;       // Total yawn count since start
+        double yawnFrequency;   // Yawns in the last 60 seconds
         double yawnDuration;
-        double yawnFrequency;
         int yawnKSS;
     };
 
-    // YawnMetrics run(const cv::Mat& frame, const std::vector<cv::Point>& faceLandmarks);
     YawnMetrics run(const std::vector<cv::Point>& faceLandmarks, int frame_width, int frame_height);
 
 private:
-    // void _draw_annotations(const cv::Mat& frame, cv::Point mouth_top, cv::Point mouth_bottom);
     int calculateYawnKSS();
     void resetKSS();
-    // double calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, cv::Size frame_size);
     double calculatePixelDistance(cv::Point landmark1, cv::Point landmark2, int frame_width, int frame_height);
     double calculateYawnFrequency(double current_time);
 
@@ -37,17 +90,18 @@ private:
     const int YAWN_THRESHOLD;
     const double MIN_YAWN_DURATION;
     const double YAWN_COOLDOWN;
-    std::deque<double> yawn_timestamps;
+    std::deque<double> yawn_timestamps;  // For 60-second frequency
     std::vector<std::map<std::string, double>> yawns;
     bool is_yawn;
     time_t yawn_start_time;
     double last_yawn_time;
     int yawn_kss;
-    double yawn_frequency;
+    double yawn_frequency;  // Yawns in last 60 seconds
     double last_yawn_duration;
     double yawn_duration;
     double reset_interval;
     double last_reset_time;
+    double totalYawnCount;  // New: Total yawns since start
 };
 
 #endif // YAWNDETECTOR_HPP
